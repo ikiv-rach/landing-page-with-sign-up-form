bcd65d7737882b734bb80d50adc3d8803c1f4871
ikiv-rach <victoriasalmi18@gmail.com>
2021-04-16 21:02:52 +0100

    cleaning up

--- sign-up-form.js
+++ sign-up-form.js
@@ -5,17 +5,12 @@ const firstname = document.getElementById('fname');
 const lastname = document.getElementById('lname');
 const phonenumber = document.getElementById('pnumber');
 
-// The following is a trick to reach the next sibling Element node in the DOM
-// This is dangerous because you can easily build an infinite loop.
-// In modern browsers, you should prefer using element.nextElementSibling
+
 let error = email, firstname, lastname, phonenumber;
 while ((error = error.nextSibling).nodeType != 1);
 
-// As per the HTML5 Specification
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-// Many legacy browsers do not support the addEventListener method.
-// Here is a simple way to handle this; it's far from the only one.
 function addEvent(element, event, callback) {
   let previousEventCallBack = element["on"+event];
   element["on"+event] = function (e) {
@@ -32,9 +27,6 @@ function addEvent(element, event, callback) {
   }
 };
 
-// Now we can rebuild our validation constraint
-// Because we do not rely on CSS pseudo-class, we have to 
-// explicitly set the valid/invalid class on our email field
 addEvent(window, "load", function () {
   // Here, we test if the field is empty (remember, the field is not required)
   // If it is not, we check if its content is a well-formed e-mail address.
